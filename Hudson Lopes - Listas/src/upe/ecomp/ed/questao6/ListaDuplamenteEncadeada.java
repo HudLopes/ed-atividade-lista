d58fca3628847202f306f045741767bd2c638b12
HudLopes <hgl@ecomp.poli.br>
2019-08-29 21:43:49 -0300

    finalizando questão 6

--- Hudson Lopes - Listas/src/upe/ecomp/ed/questao6/ListaDuplamenteEncadeada.java
+++ Hudson Lopes - Listas/src/upe/ecomp/ed/questao6/ListaDuplamenteEncadeada.java
@@ -1,8 +1,11 @@
 package upe.ecomp.ed.questao6;
 
+import upe.ecomp.ed.questao2.ListaEstatica;
+
 public class ListaDuplamenteEncadeada {
 
 	No inicio;
+	int qtd = 0;
 
 	public void add(String elemento) {
 		No novo = new No();
@@ -11,6 +14,7 @@ public class ListaDuplamenteEncadeada {
 			novo.setProximo(null);
 			novo.setAnterior(novo);
 			this.inicio = novo;
+			qtd++;
 		} else {
 			No tmp = inicio;
 			while (tmp.getProximo() != null) {
@@ -19,31 +23,34 @@ public class ListaDuplamenteEncadeada {
 			tmp.setProximo(novo);
 			novo.setAnterior(tmp);
 			novo.setProximo(null);
+			qtd++;
 		}
 	}
 
 	public boolean remover(String elemento) {
 		boolean removido = false;
-		if(isEmpty()) {
+		if (isEmpty()) {
 			return removido = false;
-		} else if(inicio.getElemento() == elemento){
+		} else if (inicio.getElemento() == elemento) {
 			inicio = inicio.getProximo();
 		} else {
 			No tmp = inicio;
-			while(tmp != null) {
-				if(tmp.getProximo().getElemento() == elemento) {
+			while (tmp != null) {
+				if (tmp.getProximo().getElemento() == elemento) {
 					tmp.setProximo(tmp.getProximo().getProximo());
-					if(tmp.getProximo() == null) {
+					if (tmp.getProximo() == null) {
+						qtd--;
 						return removido = true;
 					} else {
 						tmp.getProximo().setAnterior(tmp);
+						qtd--;
 						return removido = true;
 					}
 				}
 				tmp = tmp.getProximo();
 			}
 		}
-		
+
 		return removido;
 	}
 
@@ -57,5 +64,40 @@ public class ListaDuplamenteEncadeada {
 			System.out.print(tmp.getElemento() + " -> ");
 			tmp = tmp.getProximo();
 		}
+	}
+
+	public String consultarIndex(int index) {
+		String elemento = "";
+
+		if (index > qtd - 1) {
+			return "index maior que o tamanho da lista";
+		} else {
+			No tmp = inicio;
+			while (index > 0) {
+				tmp = tmp.getProximo();
+				index--;
+			}
+			elemento = tmp.getElemento();
+
+			return elemento;
+		}
+	}
+
+	public ListaDuplamenteEncadeada elementosEmComum(ListaDuplamenteEncadeada lista) {
+		ListaDuplamenteEncadeada listaEmComum = new ListaDuplamenteEncadeada();
+
+		No thisTmp = inicio;
+		while (thisTmp != null) {
+			No listaTmp = lista.inicio;
+			while (listaTmp != null) {
+				if (thisTmp.getElemento() == listaTmp.getElemento()) {
+					listaEmComum.add(thisTmp.getElemento());
+				}
+				listaTmp = listaTmp.getProximo();
+			}
+			thisTmp = thisTmp.getProximo();
+		}
+
+		return listaEmComum;
 	}
 }
--- Hudson Lopes - Listas/src/upe/ecomp/ed/questao6/Teste.java
+++ Hudson Lopes - Listas/src/upe/ecomp/ed/questao6/Teste.java
@@ -5,18 +5,48 @@ public class Teste {
 	public static void main(String[] args) {
 		ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada();
 		
+		
+		//LETRA A
+		
 		lista.add("a");
 		lista.add("d");
 		lista.add("b");
 		lista.add("e");
 		lista.add("c");
 
-
+		lista.print();
+		System.out.print("\n\n\n\n");
+		
+		//LETRA B
+		
 		lista.remover("b");
 		lista.remover("e");
-		
 		lista.print();
-
+		System.out.print("\n\n\n\n");
+		
+		//LETRA C
+		
+		//ja esta sendo usado nos outros testes
+		//lista.print();
+		
+		//LETRA D
+		
+		System.out.print(lista.consultarIndex(2));
+		System.out.print("\n" + lista.consultarIndex(0));
+		System.out.print("\n" + lista.consultarIndex(3));
+	
+		// LETRA E
+		
+		System.out.print("\n\n\n\n");
+		ListaDuplamenteEncadeada lista2 = new ListaDuplamenteEncadeada();
+		
+		lista2.add("z");
+		lista2.add("a");
+		lista2.add("b");
+		lista2.add("c");
+		
+		ListaDuplamenteEncadeada lista3 = lista.elementosEmComum(lista2);
+		lista3.print();
 	}
 
 }
